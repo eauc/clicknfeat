3c1e96adc4085ace5bc6c1ae6ae2fc072f9c7363
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2015-11-27 11:11:13 +0100

    Game: update tools controller

--- client/js/controllers/game/mainCtrl.js
+++ client/js/controllers/game/mainCtrl.js
@@ -13,19 +13,17 @@ angular.module('clickApp.controllers')
       console.log('init gameMainCtrl');
 
       $scope.doUseRuler = function doUseRuler() {
-        $scope.doModeAction('enterRulerMode');
+        if($scope.currentModeIs('Ruler')) {
+          $scope.doModeAction('modeBackToDefault');
+        }
+        else {
+          $scope.doModeAction('enterRulerMode');
+        }
       };
       $scope.doToggleShowRuler = function doToggleShowRuler() {
-        gameService.executeCommand('setRuler', 'toggleDisplay',
-                                   $scope,  $scope.game);
+        $scope.doExecuteCommand('setRuler', 'toggleDisplay');
       };
-      if(R.exists($scope.game)) {
-        $scope.ruler_displayed = gameRulerService.isDisplayed($scope.game.ruler);
-      }
-      $scope.onGameEvent('changeRemoteRuler', function onChangeRemoteRuler(event, ruler) {
-        $scope.ruler_displayed = gameRulerService.isDisplayed({ remote: ruler });
-        $scope.deferDigest($scope);
-      }, $scope);
+      $scope.digestOnGameEvent('changeRemoteRuler', $scope);
       
       $scope.doCreateTemplate = function doCreateTemplate(type) {
         $scope.create.template = { type: type, x: 240, y: 240 };
