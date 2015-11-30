293aa8a83d8d9cfa8564f86d2bbd072645a0b5fd
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2015-11-30 13:00:10 +0100

    Refactor: http

--- client/js/services/http.js
+++ client/js/services/http.js
@@ -1,43 +1,46 @@
 'use strict';
 
-self.httpServiceFactory = function httpServiceFactory() {
-  function ajaxRequest(method, url, data) {
-    return new self.Promise(function(resolve, reject) {
-      var client = new self.XMLHttpRequest();
-      client.open(method, url);
-      client.responseType = 'json';
-      if(R.exists(data)) {
-        client.send(JSON.stringify(data));
+angular.module('clickApp.services')
+  .factory('http', [
+    function httpServiceFactory() {
+      function ajaxRequest(method, url, data) {
+        return new self.Promise(function(resolve, reject) {
+          var client = new self.XMLHttpRequest();
+          client.open(method, url);
+          client.responseType = 'json';
+          if(R.exists(data)) {
+            client.send(JSON.stringify(data));
+          }
+          else {
+            client.send();
+          }
+          client.onload = function ajaxOnLoad() {
+            if(client.status === 200) {
+              resolve(client.response);
+            }
+            else {
+              reject(client.statusText);
+            }
+          };
+          client.onerror = function ajaxOnError() {
+            reject(client.statusText);
+          };
+        });
       }
-      else {
-        client.send();
-      }
-      client.onload = function ajaxOnLoad() {
-        if(client.status === 200) {
-          resolve(client.response);
-        }
-        else {
-          reject(client.statusText);
-        }
+      var httpService = {
+        get: function httpGet(url) {
+          return ajaxRequest('GET', url);
+        },
+        post: function httpGet(url, data) {
+          return ajaxRequest('POST', url, data);
+        },
+        put: function httpGet(url, data) {
+          return ajaxRequest('PUT', url, data);
+        },
+        delete: function httpGet(url) {
+          return ajaxRequest('DELETE', url);
+        },
       };
-      client.onerror = function ajaxOnError() {
-          reject(client.statusText);
-      };
-    });
-  }
-  var httpService = {
-    get: function httpGet(url) {
-      return ajaxRequest('GET', url);
-    },
-    post: function httpGet(url, data) {
-      return ajaxRequest('POST', url, data);
-    },
-    put: function httpGet(url, data) {
-      return ajaxRequest('PUT', url, data);
-    },
-    delete: function httpGet(url) {
-      return ajaxRequest('DELETE', url);
-    },
-  };
-  return httpService;
-};
+      return httpService;
+    }
+  ]);
